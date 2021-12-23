7bff129544387a318912984a38143d95780d520b
Bruno Higino de Souza <brunohigino06@gmail.com>
2021-12-23 19:41:32 -0300

    23/12/2021

---
+++ main.tf
@@ -0,0 +1,121 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
+#Network
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+resource "aws_default_subnet" "default_az1" {
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Default subnet for us-east-1a"
+  }
+}
+
+resource "aws_default_subnet" "default_az2" {
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Default subnet for us-east-1b"
+  }
+}
+
+# ELBSG
+resource "aws_security_group" "ELBSG" {
+  name        = "ELBSG"
+  description = "ELBSG"
+  vpc_id      = aws_default_vpc.default.id  
+}
+
+resource "aws_security_group_rule" "AllowAllIngressELB" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "all"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ELBSG.id
+}
+
+resource "aws_security_group_rule" "AllowAllEgressELB" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "all"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ELBSG.id
+}
+
+#FrontEndSG
+resource "aws_security_group" "FrontEndSG" {
+  name        = "FrontEndSG"
+  description = "FrontEndSG"
+  vpc_id      = aws_default_vpc.default.id  
+}
+
+resource "aws_security_group_rule" "AllowAllIngressELB" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "all"
+  cidr_blocks       = [aws_security_group.ELBSG.cidr_blocks]
+  security_group_id = aws_security_group.FrontEndSG.id
+}
+
+resource "aws_security_group_rule" "AllowAllEgressELB" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "all"
+  cidr_blocks       = [aws_security_group.ELBSG.cidr_blocks]
+  security_group_id = aws_security_group.FrontEndSG.id
+}
+
+# ELB
+
+resource "aws_lb" "FronEndELB" {
+  name               = "FronEndELB"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.ELBSG.id]
+  subnets            = [aws_default_subnet.default_az1, aws_default_subnet.default_az1]
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+# Instances
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+}
+
+resource "aws_instance" "FrontEnd" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  security_groups = [aws_security_group.FrontEndSG.id]
+  subnet_id = [aws_default_subnet.default_az1]
+  user_data = "${file("installnginx.sh")}"
+
+  tags = {
+    Name = "FrontEnd1"
+  }
+}
+
+resource "aws_instance" "FrontEnd" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  security_groups = [aws_security_group.FrontEndSG.id]
+  subnet_id = [aws_default_subnet.default_az2]
+  user_data = "${file("installnginx.sh")}"
+  
+  tags = {
+    Name = "FrontEnd2"
+  }
+}
